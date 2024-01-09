4ec36658f4a5c08587cc0224dbe36531545c94e5
1234 <a1234@1234-MacBook-Pro.local>
2024-01-09 14:02:20 +0200

    added final_product, datatset finished the train.py

--- Codeing_GPT/train.py
+++ Codeing_GPT/train.py
@@ -71,9 +71,9 @@ for b in range(batch_size): # batch dimension
         print(f"when input is {context.tolist()} the target: {target}")
 
 
-import torch 
+import torch
 import torch.nn as nn
-from torch.nn import functional as F 
+from torch.nn import functional as F
 torch.manual_seed(1337)
 
 class BigramLanguageModel(nn.Module):
@@ -83,39 +83,62 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
 
-        def foward(self, idx, targets=None):
+    def forward(self, idx, targets=None):
 
-            # idx and targets are bot (B, T) tensor of intergers
-            logits = self.token_embedding_table(idx) # (B,T,C)
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx) # (B,T,C)
+        
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
 
-            if targets is None:
-                lose = None
-            else:
-                B, T, C = logits.shape
-                logits = logits.view(B*T, C)
-                targets = targets.view(B*T)
-                loss = F.cross_entropy(logits, targets)
-           
-            return logits, loss
-        
-        def generate(self, idx, max_new_tokens):
-            # idx is (B, T) array of indices in the current contexct
-            for _ in range(max_new_tokens):
-                # get the predictions
-                logits, loss = self(idx)
-                #focus only on the last time setup
-                logits = logits[:, -1, :] # becomes (B, C)
-                # apply softmax to get probabilities
-                probs = F.softmax(logits, dim=-1) #(B,C)
-                # sample from th distribution
-                idx_next = torch.multinomial(probs, num_smaple=1) # (B, 1)
-                # append sampled index to the running sequnce
-                idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
-            return idx
-        
+        return logits, loss
+    
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # get the predictions
+            logits, loss = self(idx)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+
 m = BigramLanguageModel(vocab_size)
 logits, loss = m(xb, yb)
 print(logits.shape)
 print(loss)
 
 print(decode(m.generate(idx = torch.zeros((1, 1), dtype=torch.long), max_new_tokens=100)[0].tolist()))
+
+# Creating Pytorch optimizer
+optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+
+batch_size = 32
+for steps in range(10000):
+
+    #sample a batch of data
+    xb, yb = get_batch('train')
+
+    #evaluating the loss
+    logits, loss =m(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+    print(loss.item())
+
+    print(f"Step {steps + 1} - Loss: {loss.item()}")
+
+    # Print the generated result for the last iteration
+    if steps == 9999:
+        print(decode(m.generate(idx = torch.zeros((1, 1), dtype=torch.long), max_new_tokens=100)[0].tolist()))
